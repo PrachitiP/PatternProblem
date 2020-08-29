7c956e1844b83a1b14ad15981f74980d0daf1260
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 15:16:38 +0530

    Check First name is valid or not

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "User Registration"
+
+#UseCase 1=> Check First Name is valid or not
+shopt -s extglob
+
+pat="^[[:upper:]]{1}[[:lower:]]{2,8}$";
+
+#user input
+read -p "Enter First Name:" fname
+
+if [[ $fname =~ $pat ]]
+then
+        echo "First Name is valid"
+else
+        echo "First Name is not valid"
+fi
